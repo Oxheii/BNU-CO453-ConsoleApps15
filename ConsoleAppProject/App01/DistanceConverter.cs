e3bf25eaef8ccba10512b1f68669389e62ae3886
Richard Ochei Okon <91742457+Oxheii@users.noreply.github.com>
2022-02-08 04:00:39 +0000

    Update Distance Converter
    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,42 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Application to convert Distances
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Richard Okon Ochei Version 0.1
     /// </author>
+    
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
     }
 }
