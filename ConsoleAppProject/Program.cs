15710eaa11de2bff131483bb152136f4124bb038
Richard Ochei <91742457+Oxheii@users.noreply.github.com>
2022-02-20 15:17:52 +0000

    Program updated
    Fixing program.cs as i couldnt utilise the CMD

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleAppProject.App03;
+﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App03;
 using ConsoleAppProject.Helpers;
 using System;
 
@@ -7,19 +8,28 @@ namespace ConsoleAppProject
     /// <summary>
     /// The main method in this class is called first
     /// when the application is started.  It will be used
-    /// to start Apps 01 to 05 for CO453 CW1
+    /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Richard Ochei 20/02/2022
     /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            
-            Console.WriteLine("BNU CO453 Applications Programming 2021-2022!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+
             Console.WriteLine();
+            Console.WriteLine(" =================================================");
+            Console.WriteLine("   BNU CO453 Applications Programming 2021-2022  ");
+            Console.WriteLine("                by Richard Ochei                  ");
+            Console.WriteLine(" =================================================");
+            Console.WriteLine();
+
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
+            Console.Beep();
         }
     }
 }
