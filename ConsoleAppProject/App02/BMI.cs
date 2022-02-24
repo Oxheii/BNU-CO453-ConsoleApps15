6257c480561dbce634816db5d3ef3394b987817f
Richard Ochei <91742457+Oxheii@users.noreply.github.com>
2022-02-24 23:29:45 +0000

    Updated BMI calc

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+
+namespace ConsoleAppProject.App02
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// BMI | Calculator 
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Richard Ochei -  version 0.1
     /// </author>
     public class BMI
     {
